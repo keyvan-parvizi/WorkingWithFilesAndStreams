964540bbbe9f001081c6fa975b965ebf04518b74
K1 <K1@DESKTOP-VJ74ISC>
2023-05-01 22:19:39 +0430

    Managing Drives Sample

---
+++ ManagingDrives/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+WorkingWithDrives();
+
+static void WorkingWithDrives()
+{
+    WriteLine($"{0, -30} | {1, -10} | {2, -7} | {3, 18} | {4, 18}",
+        "Name", "Type", "Format", "Size (Bytes)", "Free Space");
+
+    foreach (DriveInfo drive in DriveInfo.GetDrives())
+    {
+        if (drive.IsReady)
+        {
+            WriteLine($"{0,-30} | {1,-10} | {2,-7} | {3,18:NO} | {4,18:NO}",
+                drive.Name, drive.DriveType, drive.DriveFormat, drive.TotalSize, drive.AvailableFreeSpace);
+        }
+        else
+        {
+            WriteLine($"{0, -30} | {1, -10}", drive.Name, drive.DriveType);
+        }
+    }
+}
