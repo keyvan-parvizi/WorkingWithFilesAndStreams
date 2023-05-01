c2de8b193cd10040baa8c1b201da185f9a318694
K1 <K1@DESKTOP-VJ74ISC>
2023-05-01 23:16:09 +0430

    Managing Paths

--- ManagingFiles/Program.cs
+++ ManagingFiles/Program.cs
@@ -42,4 +42,12 @@ static void WorkingWithFiles()
     StreamReader textReader = File.OpenText(backupFile);
     WriteLine(textReader.ReadToEnd());
     textReader.Close();
+
+    //Managing Paths
+    WriteLine($"Folder Name: {GetDirectoryName(textFile)}");
+    WriteLine($"File Name: {GetFileName(textFile)}");
+    WriteLine($"File Name without Extension: {GetFileNameWithoutExtension(textFile)}");
+    WriteLine($"File Extension: {GetExtension(textFile)}");
+    WriteLine($"Random File Name: {GetRandomFileName()}");
+    WriteLine($"Temporary File Name: {GetTempFileName()}");
 }
