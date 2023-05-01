258b52d25ecf225ccb4b3d9e1b80f31473bca6c9
K1 <K1@DESKTOP-VJ74ISC>
2023-05-01 22:43:42 +0430

    Managing Directories

---
+++ ManagingDirectories/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+WorkingWithDirectories();
+
+static void WorkingWithDirectories()
+{
+    //define a directory path for a new folder
+    //starting in the user's folder
+    string newFolder = Combine(GetFolderPath(SpecialFolder.Personal), "Code", "NewFolder");
+
+    WriteLine($"Working with: {newFolder}");
+
+    //check if it exists
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+
+    //create directory
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+
+    //delete directory
+    WriteLine("Deleting it...");
+    Delete(newFolder, true);
+    WriteLine($"Does it exist? {Exists(newFolder)}");
+}
