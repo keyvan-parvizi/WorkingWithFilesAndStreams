b6d3550f43af3a97e71e52296c2c5fe8cda75eb1
K1 <parvizi.k1@hotmail.com>
2023-05-03 17:40:50 +0430

    writing to XML streams

--- WritingToTextStream/Program.cs
+++ WritingToTextStream/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+WorkingWithText();
+
+static void WorkingWithText()
+{
+    var names = new string[] { "X", "Y", "Z" };
+
+    //define a file to write to
+    string textFile = Combine(GetFolderPath(SpecialFolder.Personal), "streams.text");
+
+    //create a text file and return a helper writer
+    StreamWriter text = File.CreateText(textFile);
+
+    //enumarate the strings, writing each one to the stream on a sparate line
+    foreach (var name in names)
+    {
+        text.WriteLine(name);
+    }
+    text.Close();
+
+    WriteLine($"{textFile} contains {new FileInfo(textFile).Length} bytes.");
+
+    WriteLine(File.ReadAllText(textFile));
+}
